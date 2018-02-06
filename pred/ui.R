6e34209123573092a975e065a7e288d022fb3b8f
Niall Tyndall <ntyndall@gmail.com>
2018-02-06 22:50:13 +0000

    update ui component

--- pred/ui.R
+++ pred/ui.R
@@ -1,10 +1,34 @@
 library(shiny)
 
-# Define UI for application that plots random distributions 
+# Define UI for application that plots random distributions
 shinyUI(fluidPage(
 
+  # App CSS ----
+  #theme = "bootstrap.css",
+
+  tags$head(
+    tags$style(HTML("
+      @import url('//fonts.googleapis.com/css?family=Arvo|VT323');
+
+      body, html {
+        background-color: #212121;
+        font-family: 'Arvo', serif;
+        font-weight: 700;
+        line-height: 1.1;
+        color: #FFFFFF;
+      }
+
+      #sidebar {
+        background-color: #F5F5F5;
+        color: #000000;
+      }
+    "))
+  ),
+
   # App title ----
-  titlePanel("Predictions & Results"),
+  #titlePanel("Select Options"),
+
+  headerPanel("Select Options"),
 
   # Sidebar layout with a input and output definitions ----
   sidebarLayout(
@@ -12,18 +36,20 @@ shinyUI(fluidPage(
     # Sidebar panel for inputs ----
     sidebarPanel(
 
+      id = 'sidebar',
+
       # Input: Selector for choosing dataset ----
       selectInput(inputId = "league",
                   label = "Choose a competition:",
                   choices = c("premier-league", "championship", "france")),
 
       selectInput(inputId = "season",
-                   label = "Select season (2017='17/'18)",
+                  label = "Select season (2017='17/'18)",
                   choices = c("2017/2018", "2018/2019")),
 
       # Input: Numeric entry for number of obs to view ----
       selectInput(inputId = "month",
-                   label = "Month to look at",
+                  label = "Month to look at",
                   choices = c("January", "February"))
     ),
 
